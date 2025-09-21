1c0dbdcf49c1229c5875982d183570203e321e7b
Anmol Jadgilwar <developer.anmol108@gmail.com>
2025-09-21 21:14:12 +0530

    fit: dark mode improved

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  darkMode: 'class', // Enable dark mode with class strategy
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  dark: "class", // Enable dark mode with class strategy
   theme: {
     extend: {},
   },
-  plugins: [
-    require('tailwind-scrollbar-hide')
-  ],
-}
+  plugins: [require("tailwind-scrollbar-hide")],
+};
